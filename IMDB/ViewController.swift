23e02c891df5982d2218b176a75fa07eb971ba09
Ramy Sabry <ramy.sabry.external@atos.net>
2022-04-06 14:23:49 +0200

    Initial Commit

---
+++ IMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDB
+//
+//  Created by Ramy Sabry on 06/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
