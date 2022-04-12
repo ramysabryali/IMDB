e75d772974389bb9ef718e260184c823f46613f6
Ramy Sabry <ramysabry1996@gmail.com>
2022-04-13 01:06:23 +0200

    enhance fetching top rated movies

--- IMDB/Home Module/Home/ViewModel/HomeViewModel.swift
+++ IMDB/Home Module/Home/ViewModel/HomeViewModel.swift
@@ -5,11 +5,14 @@
 //  Created by Ramy Sabry on 11/04/2022.
 //
 
-import Foundation
 import RxSwift
 
 final class HomeViewModel: BaseViewModel {
     private let apiService: APIServiceContract
+    private let pageSize: Int = Constants.pageSize
+    private var page: Int = 1
+    
+    var topRatedMovies: [MovieData] = []
     
     init(apiService: APIServiceContract = APIService.shared) {
         self.apiService = apiService
@@ -18,6 +21,14 @@ final class HomeViewModel: BaseViewModel {
     
     func fetchData() {
         //        .setPath(using: .topRated, argument: ["\(1)", "\(10)"])
+        fetchTopRatedMovies()
+    }
+}
+
+private extension HomeViewModel {
+    func fetchTopRatedMovies() {
+        stateRelay.accept(.loading)
+        
         let request = APIBuilder()
             .setPath(using: .topRated)
             .setMethod(using: .get)
@@ -26,14 +37,22 @@ final class HomeViewModel: BaseViewModel {
         
         apiService.request(using: request, responseType: MoviesResponse.self)
             .subscribe(
-                onNext: { result in
-                    switch result {
-                    case .success(let movies):
-                        dump(movies)
-                        
-                    case .failure(let error):
-                        print(error.localizedDescription)
+                onNext: { [weak self] result in
+                    guard let self = self else { return }
+                    
+                    if case let .failure(error) = result {
+                        self.stateRelay.accept(.failed(error))
+                        self.alertItemRelay.accept(.init(message: error.message))
+                        return
                     }
+                    
+                    guard
+                        case let .success(response) = result,
+                        let topRatedMovies: [MovieData] = response.results
+                    else { return }
+                    
+                    self.topRatedMovies.append(contentsOf: topRatedMovies)
+                    self.stateRelay.accept(.successful)
                 }
             ).disposed(by: disposeBag)
     }
